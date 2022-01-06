64a3215be5e64a66ce660cb2e871e0edcc428129
katomikiya <kato-m@omoteya.co.jp>
2022-01-06 13:58:06 +0900

    キャッシュ削除

---
+++ ToDoApp/Models/Item.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoApp.Models
+{
+    public class Item
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+    }
+}
